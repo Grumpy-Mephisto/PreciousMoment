f4722d854abfcdbee9fdae85dee5aa086a3dc763
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-03-08 15:14:47 +0700

    🎨 style: formatting

--- src/systems/render_system.cpp
+++ src/systems/render_system.cpp
@@ -12,8 +12,8 @@ void RenderSystem::update(
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   for (auto &[entity, renderable] : renderComponents) {
-
     TransformComponent &transform = transformComponents[entity];
+
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, transform.position);
     model = glm::rotate(model, glm::radians(transform.eulers.z),
